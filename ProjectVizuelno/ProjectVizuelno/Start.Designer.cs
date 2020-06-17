f64f33a08a65d471df899713c768b4b7e18df90f
Darko Skerlevski <darkoskerlevski@gmail.com>
2020-06-17 13:37:52 +0200

    leaderboard implemented

--- ProjectVizuelno/ProjectVizuelno/Start.Designer.cs
+++ ProjectVizuelno/ProjectVizuelno/Start.Designer.cs
@@ -34,10 +34,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new ProjectVizuelno.NoSelectButton();
+            this.button4 = new ProjectVizuelno.NoSelectButton();
+            this.button5 = new ProjectVizuelno.NoSelectButton();
+            this.button6 = new ProjectVizuelno.NoSelectButton();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -137,11 +138,22 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(151, 220);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Излези";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 249);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -167,9 +179,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private NoSelectButton button3;
+        private NoSelectButton button4;
+        private NoSelectButton button5;
+        private NoSelectButton button6;
     }
 }
--- ProjectVizuelno/ProjectVizuelno/Start.cs
+++ ProjectVizuelno/ProjectVizuelno/Start.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,22 @@ using System.Windows.Forms;
 
 namespace ProjectVizuelno
 {
+
     public partial class Start : Form
     {
         public Start()
         {
             InitializeComponent();
-           /* button3.CanSelect = false;
-            button4.CanSelect = false;
-            button5.CanSelect = false;
-            button6.CanSelect = false;
-            button7.CanSelect = false; */
+            /*if (!(File.Exists("leaderboard.txt")))  // ENABLE THIS WHEN EVERYTHING ELSE IS DONE
+            {
+                var newFile = File.Create("leaderboard.txt");
+                newFile.Close();
+            } */
         }
 
-        private void reEnableButtons(Button skip)
+        private bool tezina = false; // bool promenliva za dali e stisnato na edna od tezinite
+
+        private void reEnableButtons(Button skip) // dokolku se stisne edno kopce za difficulty, gi enable-nuva ostanatite
         {
             Button[] buttons = { button3, button4, button5, button6};
             foreach(Button bttn in buttons)
@@ -32,15 +36,28 @@ namespace ProjectVizuelno
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) // leaderboards button
         {
-            MessageBox.Show("Not implemented yet!", "Implementation Error", MessageBoxButtons.OK);
+            //MessageBox.Show("Not implemented yet!", "Implementation Error", MessageBoxButtons.OK);
+            leaderboard lb = new leaderboard();
+            this.Hide();
+            lb.ShowDialog();
+            this.Show();
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // "zapocni" button
         {
-            if (textBox1.TextLength == 0)
+            if (textBox1.TextLength == 0 && tezina == false) 
+            {
+                MessageBox.Show("Внесете име и тежина!", "Грешка", MessageBoxButtons.OK);
+            }
+            else if (textBox1.TextLength == 0)
                 MessageBox.Show("Мора да го внесете вашето име!", "Грешка", MessageBoxButtons.OK);
+            else if (tezina == false)
+            {
+                MessageBox.Show("Изберете тежина!", "Грешка", MessageBoxButtons.OK);
+            }
             else if (textBox1.TextLength <=2)
             {
                 MessageBox.Show("Вашето име е прекратко!", "Грешка", MessageBoxButtons.OK);
@@ -55,33 +72,49 @@ namespace ProjectVizuelno
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e) //difficulty button 1
         {
+            tezina = true;
             button3.Enabled = false;
             reEnableButtons(button3);
             button3.Focus();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e) //difficulty button 2
         {
+            tezina = true;
             button4.Enabled = false;
             reEnableButtons(button4);
             this.Focus();
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e) //difficulty button 3
         {
+            tezina = true;
             button5.Enabled = false;
             reEnableButtons(button5);
             this.Focus();
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e) //difficulty button 4
         {
+            tezina = true;
             button6.Enabled = false;
             reEnableButtons(button6);
             this.Focus();
         }
 
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    class NoSelectButton : Button // klasa za kopcinja sto ne moze da se selektiraat (select fix)
+    {
+        public NoSelectButton()
+        {
+            SetStyle(ControlStyles.Selectable, false);
+        }
     }
 }
